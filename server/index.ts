75069430644e569d8572416f9d28a0691ae2faa7
samanpreets84 <132365157+Saman-dev12@users.noreply.github.com>
2025-01-22 14:46:37 +0530

    backend

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,6 @@ wss.on("connection", (ws: WebSocket) => {
     console.log("New connection");
     ws.on("message", (data: any) => {
         const message = JSON.parse(data);
-        console.log("Message: ", JSON.parse(data.toString()));
         // console.log("Message: ", message);
         wss.clients.forEach((client: WebSocket) => {
             if (client !== ws && client.readyState === WebSocket.OPEN) {
